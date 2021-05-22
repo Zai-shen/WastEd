282b1efe609d0780859b52c0f85296ecef3c1a74
Miran Jank <e1526438@mail.student.tuwien.ac.at>
2021-05-22 18:49:18 +0200

    Dash bugfix - no longer clipping through walls

--- Assets/First person controller/FirstPersonMovement.cs
+++ Assets/First person controller/FirstPersonMovement.cs
@@ -3,35 +3,42 @@ using UnityEngine;
 
 public class FirstPersonMovement : MonoBehaviour
 {
+    private bool ableToMove = true;
     public float speed = 5;
-    public float dashDistance = 5f;
-    public float dashSpeed = 1f;
+
+    public float dashSpeed = 10f;
+    public float dashDuration = 0.5f;
+    public float dashCD = 5f;
+    private float nextDashTime = 0f;
+    private Rigidbody rb;
+
     public float shieldDuration = 3f;
     Vector2 velocity;
     public bool ableToShield = false;
     public bool ableToSecondLife = false;
     public bool ableToDash = false;
 
-    public bool mayShield = false;
     public bool mayDash = false;
-
-    public float shieldCD = 10f;
+    public bool mayShield = false;
     private float nextShieldTime = 0f;
-    public float dashCD = 5f;
-    private float nextDashTime = 0f;
+    public float shieldCD = 10f;
 
     private PlayerHPHandler pHPHandler;
 
     private void Start()
     {
         pHPHandler = GetComponent<PlayerHPHandler>();
+        rb = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
     {
-        velocity.y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        velocity.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        transform.Translate(velocity.x, 0, velocity.y);
+        if (ableToMove)
+        {
+            velocity.y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+            velocity.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+            transform.Translate(velocity.x, 0, velocity.y);
+        }
     }
 
     private void Update()
@@ -58,20 +65,44 @@ public class FirstPersonMovement : MonoBehaviour
     /// Currently sets the transform - which allows for precise control. Though it is possible to clip through walls/etc.
     /// TODO: make as addForce() and for the duration eliminate friction.
     /// </summary>
+    //IEnumerator DashLegacy()
+    //{
+    //    Vector3 a = transform.position;
+    //    Vector3 b = new Vector3(transform.position.x, transform.position.y, transform.position.z) + transform.forward * dashDistance;
+
+    //    float step = (dashSpeed / (a - b).magnitude) * Time.fixedDeltaTime;
+    //    float t = 0;
+    //    while (t <= 1)
+    //    {
+    //        t += step; // Goes from 0 to 1, incrementing by step each time
+    //        transform.position = Vector3.Lerp(a, b, t); // Move objectToMove closer to b
+    //        yield return new WaitForFixedUpdate();         // Leave the routine and return here in the next frame
+    //    }
+    //    transform.position = b;
+    //}
+
     IEnumerator Dash()
     {
-        Vector3 a = transform.position;
-        Vector3 b = new Vector3(transform.position.x, transform.position.y, transform.position.z) + transform.forward * dashDistance;
+        //Disable movement
+        ableToMove = false;
 
-        float step = (dashSpeed / (a - b).magnitude) * Time.fixedDeltaTime;
-        float t = 0;
-        while (t <= 1)
+        //Get start rotation
+        Vector3 startRot = transform.forward;
+
+        //Add rb force in direction
+        float t = 0f;
+        while (t <= dashDuration)
         {
-            t += step; // Goes from 0 to 1, incrementing by step each time
-            transform.position = Vector3.Lerp(a, b, t); // Move objectToMove closer to b
-            yield return new WaitForFixedUpdate();         // Leave the routine and return here in the next frame
+            t += Time.fixedDeltaTime; // Adds frame time
+            rb.velocity = startRot * dashSpeed; // Set constant force
+            yield return new WaitForFixedUpdate(); // Leave the routine and return here in the next frame
         }
-        transform.position = b;
+
+        //Then reset velocity
+        rb.velocity = new Vector3(0,0,0);
+
+        //Enable movement
+        ableToMove = true;
     }
 
 }
